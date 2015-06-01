05d3684d34a8437090a76f56898b501525fb275a
Kenneth Ho <a0125364@u.nus.edu>
2015-06-01 15:44:03 +0800

    changed logout button bottom padding added textbox label toggle javascript changed home button link

--- javascripts/default.js
+++ javascripts/default.js
@@ -1,9 +1,51 @@
 // JavaScript Document
 
+/***
+*	Initialise elements in the login page.
+*/
 function initLogin() {
+	loginHandler();
+}
+
+/***
+*	Event Handler for elements in login page.
+*/
+function loginHandler() {
+	$("#id-textbox").focus(function() {
+		toggleLabel(this);
+	});
+	
+	$("#id-textbox").blur(function() {
+		toggleLabel(this);
+	});
+	
+	$("#password-textbox").focus(function() {
+		toggleLabel(this);
+	});
+	
+	$("#password-textbox").blur(function() {
+		toggleLabel(this);
+	});
+}
+
+/***
+*	Initialise elements in the index page.
+*/
+function initPage() {
 	
 }
 
-function initPage() {
+/***
+*	Toggle the label of a textbox when that particular textbox gains focus.
+*
+*	element: the element to have its' label toggled
+*	ignoreSpace: treat space as empty
+*/
+function toggleLabel(element) {
+	var content = $(element).val();
 	
+	if(!$.trim(content)) {
+		$(element).siblings("label").toggle();
+		$(element).val("");	
+	}
 }
